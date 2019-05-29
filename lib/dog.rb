62be8e282f9f1e4d1f3240b5c0300f2c80559fa4
Tom White <eugene.white@gmail.com>
2019-05-29 19:56:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,19 @@
 class Dog
 
-  def initialize(dogs_name)
-      @name = dogs_name
-    end
-
   def name=(dogs_name)
-    @dog_name = dogs_name
+    @name = dogs_name
   end
 
   def name
-    @dog_name
+    @name
   end
 
   def breed=(dogs_breed)
-    @dog_breed = dogs_breed
+    @breed = dogs_breed
   end
 
   def breed
-    @dog_breed
+    @breed
   end
 
 end
