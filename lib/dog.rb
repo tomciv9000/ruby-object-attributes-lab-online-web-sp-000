1bc3c971a15cbbb24fb0e2d88d17a8cf60c85817
Tom White <eugene.white@gmail.com>
2019-05-29 19:51:43 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name=(dog_name)
+    @dog_name = dog_name
+  end
+
+  def name
+    @dog_name
+  end
+
+  def breed=(dog_breed)
+    @dog_breed = dog_breed
+  end
+
+  def breed
+    @dog_breed
+  end
+
+end
